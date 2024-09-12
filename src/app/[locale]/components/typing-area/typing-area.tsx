28a7f3acf38ccfc0ecea720af4d683a6c280f82d
sorlros <sorlros2047@gmail.com>
2024-09-12 15:53:29 +0900

    글자수 제한 함수 변경

--- src/app/[locale]/components/typing-area/typing-area.tsx
+++ src/app/[locale]/components/typing-area/typing-area.tsx
@@ -17,6 +17,26 @@ const TypingArea = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [shakingIndex, setShakingIndex] = useState<number | null>(null);
 
+  const [visibleContent, setVisibleContent] = useState("");
+
+  const MAX_VISIBLE_CHARS = 50;
+
+  // const abc = text.content.slice(50, 200);
+  // console.log("abc", abc)
+
+  useEffect(() => {
+    setVisibleContent(text.content.slice(0, MAX_VISIBLE_CHARS));
+  }, [text])
+
+  // useEffect(() => {
+  //   if (typedText.length - 1 > visibleContent.length) {
+  //     const nextStartIndex = typedText.length;
+  //     const nextEndIndex = nextStartIndex + MAX_VISIBLE_CHARS;
+  //     setVisibleContent(text.content.slice(nextStartIndex, nextEndIndex));
+  //   }
+  // }, [typedText, visibleContent, text.content]);
+
+
   const isSpecialCharacter = (char: string) => {
     // 정규 표현식을 사용하여 알파벳, 숫자, 한글 외의 문자를 확인
     return !/^[a-zA-Z0-9가-힣\s]$/.test(char);
@@ -28,9 +48,13 @@ const TypingArea = () => {
 
     const lastIndex = userInput.length - 1;
     const currentChar = userInput[lastIndex];
-    console.log("currentChar", currentChar);
+    // console.log("currentChar", currentChar);
     const correctChar = text.content[lastIndex];
-    console.log("correctChar", correctChar);
+    // console.log("correctChar", correctChar);
+    console.log(typedText.length, MAX_VISIBLE_CHARS, "typedText.length")
+
+    // console.log("userInput.length", userInput.length);
+
 
     // const decompCurrentChar = decomposeHangul(currentChar);
     // const decompCorrectChar = decomposeHangul(correctChar);
@@ -50,12 +74,23 @@ const TypingArea = () => {
     }
   }, []);
 
+  
+
   const renderText = () => {
+    useEffect(() => {
+      if (typedText.length - 1 > visibleContent.length) {
+        console.log("이콜")
+        const nextStartIndex = typedText.length; // 입력된 문자수만큼 이후 시작
+        const nextEndIndex = nextStartIndex + MAX_VISIBLE_CHARS; // 다음 200글자 범위 설정
+        setVisibleContent(text.content.slice(nextStartIndex, nextEndIndex)); // 다음 200글자만 표시
+      }
+    }, [typedText])
+    
     return (
       <>
         {/* 유저가 입력한 부분 */}
         {typedText.split("").map((char, index) => {
-          const correctChar = text.content[index]; // 올바른 텍스트와 비교
+          const correctChar = visibleContent[index]; // 올바른 텍스트와 비교
           const isCorrect = char === correctChar;
           const color = isCorrect ? "text-white" : "text-red-500"; // 올바르면 흰색, 틀리면 빨간색
           const isShaking = shakingIndex === index ? "shake" : "";
@@ -70,7 +105,7 @@ const TypingArea = () => {
         {/* 남은 부분 */}
         <span className="text-white animate-blink border-r-2" />
 
-        {text.content.slice(typedText.length).split("").map((char, index) => (
+        {visibleContent.slice(typedText.length).split("").map((char, index) => (
           <span key={index} className="text-gray-400">
             {char}
           </span>
@@ -81,7 +116,7 @@ const TypingArea = () => {
   
   return (
     <div>
-      <div className="bg-black p-4 rounded-lg w-full h-[200px] overflow-hidden font-mono text-lg text-left leading-relaxed"
+      <div className="bg-black p-4 rounded-lg w-full h-[300px] overflow-hidden font-mono text-lg text-left leading-relaxed px-4 py-6"
       >
         <div>{renderText()}</div>
         <input
