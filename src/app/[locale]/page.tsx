4b76e3612b02a32941e74c70cc576be950de1aa0
sorlros <sorlros2047@gmail.com>
2025-02-04 14:33:27 +0900

    02

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,6 +6,15 @@ const ClientComponentPage = dynamic(() => import("./client-page"), {
   ssr: false, // 서버 사이드 렌더링 방지
 });
 
+async function fetchTextFromPublic() {
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/api/get-text`;
+
+  const response = await fetch(url, { cache: "no-store" });
+  if (!response.ok) throw new Error("get-text fetch 오류");
+
+  return response.json();
+}
+
 export default async function LocalePage() {
   // const { locale, translation } = params;
   // const validLocales = ["ko", "en"];
@@ -21,7 +30,15 @@ export default async function LocalePage() {
   // }
 
   // const lang = await fetchLangFromServer(locale);
-  const text = await fetchTextFromPublic();
+  // const text = await fetchTextFromPublic();
+
+  let text = "";
+
+  try {
+    text = await fetchTextFromPublic();
+  } catch (error) {
+    console.error("fetchTextFromPublic 실패:", error);
+  }
 
   // i18n.addResourceBundle(locale, "translation", translation, true, true);
   // i18n.changeLanguage(locale);
@@ -65,22 +82,22 @@ export default async function LocalePage() {
 // }
 
 // 서버에서 텍스트 데이터를 가져오는 함수
-async function fetchTextFromPublic() {
-  // const response = await fetch(`http://localhost:3000/api/get-text`);
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/get-text`);
+// async function fetchTextFromPublic() {
+//   // const response = await fetch(`http://localhost:3000/api/get-text`);
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/get-text`);
   
-  // const text = await response.text();
-  // console.log("fetchTextFromPublic 응답 상태 코드:", response.status);
+//   // const text = await response.text();
+//   // console.log("fetchTextFromPublic 응답 상태 코드:", response.status);
 
-  // const text = await response.text(); // JSON인지 확인하기 위해 text로 받음
-  // console.log("fetchLangFromServer 응답 내용:", text); // 응답 내용 출력
+//   // const text = await response.text(); // JSON인지 확인하기 위해 text로 받음
+//   // console.log("fetchLangFromServer 응답 내용:", text); // 응답 내용 출력
 
-  if (!response.ok) {
-    throw new Error("get-text fetch 오류");
-  }
+//   if (!response.ok) {
+//     throw new Error("get-text fetch 오류");
+//   }
 
-  // const json = JSON.parse(text);
+//   // const json = JSON.parse(text);
 
-  return response.json();
-  // return json;
-}
+//   return response.json();
+//   // return json;
+// }
